02fbfdf59409a66964f28024cc8df2ec8aa071dc
Tugus <idabgusindra01@gmail.com>
2022-12-06 21:17:17 +0800

    edit new code

--- soal.go
+++ soal.go
@@ -8,9 +8,6 @@ func main(){
 	var input1, input2, input3, input4, hasil1, hasil2 int
 
 	fmt.Scan(&input1, &input2, &input3, &input4)
-	x := input3 == input1
-	x1 := input3 == input2
-	x2 := input3 == input4
 	if ((input1 < input2) && (input1 < input3) && (input1 < input4)) && ((input2 > input1) && (input2 > input3) && (input2 > input4)) {
 		hasil1 = input2
 		hasil2 = input1
@@ -59,7 +56,7 @@ func main(){
 		hasil1 = input1
 		hasil2 = input4
 		fmt.Print(hasil1, hasil2)
-	} else if ((input1 == input2) && (input1 == input3) && (input1 == input4)) && ((input2 == input1) && (input2 == input3) && (input2 == input4)) && ((x) && (x1) && (x2)) && ((input3 == input1) && (input3 == input2) && (input3 == input4)) && ((input4 == input1) && (input4 == input3) && (input4 == input2))  {
+	} else if ((input1 == input2) && (input1 == input3) && (input1 == input4)) && ((input2 == input1) && (input2 == input3) && (input2 == input4)) && ((input3 == input1) && (input3 == input2) && (input3 == input4)) && ((input4 == input1) && (input4 == input3) && (input4 == input2))  {
 		hasil1 = input1
 		hasil2 = input1
 		fmt.Print(hasil1, hasil2)
